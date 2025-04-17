68dd58464fba6243d8c61a32b619b504da754c5a
Jchosenn <jonathanchosen79@gmail.com>
2025-04-17 16:43:40 +0100

    service.page

--- signin.js
+++ signin.js
@@ -1,7 +1,8 @@
-let siginElement = document.getElementById("sign-in");
-let signupElement = document.getElementById("sign-up");
-let btnElement = document.getElementById("btn");
-
+let siginElement = document.querySelector("#sign-in");
+let signupElement = document.querySelector("#sign-up");
+let btnElement = document.querySelector("#btn");
+let heroElement = document.querySelector(".hero");
+let webElement = document.querySelector(".web-holder")
 function signup(){
     siginElement.style.left = "-400px";
     signupElement.style.left = "50px";
@@ -11,4 +12,20 @@ function signin(){
     siginElement.style.left = "50px";
     signupElement.style.left = "450px";
     btnElement.style.left = "0";
+}
+function toggleSignin(){
+    signin();
+    heroElement.classList.add("popup");
+    // heroElement.style.display = "block";
+    webElement.style.filter = "blur(10px)";
+}
+function toggleSignup(){
+    signup();
+    heroElement.classList.add("popup");
+   // heroElement.style.display = "block";
+    webElement.style.filter = "blur(10px)";
+}
+function closeBtn(){
+    heroElement.classList.remove("popup");
+    webElement.style.filter = "blur(0px)";
 }
